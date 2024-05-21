fb0100c8d81d908238f3774c4d9ff8a9f707d6cd
denizhosgor <denizhosgor@gmail.com>
2024-05-21 22:18:51 +0300

    ok

--- kivymd/_version.py
+++ kivymd/_version.py
@@ -1,5 +1,5 @@
 release = False
 __version__ = "2.0.1.dev0"
-__hash__ = "f7bde69707ac708a758a02d89f14997ee468d1ee"
-__short_hash__ = "f7bde69"
-__date__ = "2024-02-27"
+__hash__ = "96887c8cdfff22527b3d934f207810ba93eea1fa"
+__short_hash__ = "96887c8"
+__date__ = "2024-05-20"
